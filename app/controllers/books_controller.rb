651ac15e8f12ad693b179d1736b82459e00f7411
skolesnikov <skolesnikov@gmail.com>
2014-09-25 21:07:44 -0400

    Class 4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,8 @@ class BooksController < ApplicationController
 	before_action :set_book, only: [ :show, :edit, :update, :destroy ]
 
 	def index
-		@available_at = Time.now
-		@books = Book.all
+	  @available_at = Time.now
+	  @books = Book.order(:title).page(params[:page])
 	end
 
 	def show; end
@@ -14,15 +14,21 @@ class BooksController < ApplicationController
 
 	def create
 	  @book = Book.new(book_params)
-	  @book.save
-	  redirect_to @book
+	  if @book.save
+	    redirect_to @book, notice: "#{@book.title} was created!"
+	  else
+	    render :new
+	  end
 	end
 
 	def edit; end
 
 	def update
-	  @book.update(book_params)
-	  redirect_to @book
+      if @book.update(book_params)
+        redirect_to @book, notice: "#{@book.title} was updated!"
+      else
+        render :new
+      end
 	end
 
 	def destroy
